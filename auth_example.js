0895a7865c7c5814ef0ac0c29d316dce81132dbd
Alyssa Jordan <50706948+alyssaaj@users.noreply.github.com>
2024-06-01 14:58:18 -0400

    Update auth_example.js

--- auth_example.js
+++ auth_example.js
@@ -2,3 +2,11 @@ export const username = 'amj426';
 export const password = 'aiZ1eeYenei';
 export const authDatabase = 'amj426_INFO670_202335';
 export const databaseName = 'amj426_INFO670_202335';
+
+
+module.exports = {
+  username,
+  password,
+  authDatabase,
+  databaseName
+};
